c9dd558e15069f4ba126a7a50fea7a64c5969c18
NALUBOLU RAJASEKHAR <nalubolurajasekhar7386@gmail.com>
2025-12-15 11:26:31 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "name" {
+    ami = "ami-00ca570c1b6d79f36"
+    instance_type = "t2.micro"
+    key_name = "rajabikekey"
+    tags = {
+      Name = "dev"
+    }
+   
+  
+}
